2988c28fca7482769be08f3a5c7e86e6b166a5d1
Nikita Smirnov <smirn_n@yahoo.com>
2018-07-18 00:27:53 +0300

    Fix config name

--- core/core_commands.py
+++ core/core_commands.py
@@ -41,17 +41,17 @@ class Core:
         """Changes bot's settings"""
         if ctx.invoked_subcommand is None:
             if ctx.guild:
-                admin_role_id = await ctx.bot.db.guild(ctx.guild).admin_role()
+                admin_role_id = await ctx.bot.config.guild(ctx.guild).admin_role()
                 admin_role = discord.utils.get(ctx.guild.roles, id=admin_role_id) or "Not set"
-                mod_role_id = await ctx.bot.db.guild(ctx.guild).mod_role()
+                mod_role_id = await ctx.bot.config.guild(ctx.guild).mod_role()
                 mod_role = discord.utils.get(ctx.guild.roles, id=mod_role_id) or "Not set"
-                prefixes = await ctx.bot.db.guild(ctx.guild).prefix()
+                prefixes = await ctx.bot.config.guild(ctx.guild).prefix()
                 guild_settings = f"Admin role: {admin_role}\nMod role: {mod_role}\n"
             else:
                 guild_settings = ""
                 prefixes = None  # This is correct. The below can happen in a guild.
             if not prefixes:
-                prefixes = await ctx.bot.db.prefix()
+                prefixes = await ctx.bot.config.prefix()
 
             prefix_string = " ".join(prefixes)
             settings = (
